587f8790e7c4ba3729a3ade5a9f6e3541d711dc2
Ridge <ridge.liu@toursforfun.com>
2015-01-30 17:34:10 +0800

    html halt twige var escape

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+/* global escape */
 "use strict";
 var gulp = require("gulp"),
 	path = require("path"),
@@ -268,6 +269,7 @@ function compiler(opt) {
 			return doWhenNotLock(function() {
 				return files.pipe(plumber(errrHandler))
 					.pipe(replace(/\{\s*%[\s\S]+?%\s*\}/g, ""))
+					.pipe(replace(/\{\{[\s\S]+?\}\}/g, escape))
 					.pipe(htmlhint({
 						"doctype-first": false
 					}))
